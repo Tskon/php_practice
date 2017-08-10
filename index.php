e53a27d3071e31342676052b62f492a0ecf537ff
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-08-10 22:06:53 +0300

    First lesson. Install Apache and php-7

--- index.php
+++ index.php
@@ -6,24 +6,8 @@
 </head>
 <body>
 <?php
-$connection = mysqli_connect('localhost','sctreidru_test1','q1w2Q1W2','sctreidru_test1');
-
-if( $connection == false ){
-    echo 'Connection failed!<br>';
-    echo mysqli_connect_error();
-    exit();
-}
-$result = mysqli_query($connection, "SELECT * FROM `articles_categories`");
-echo 'Find categories: ' . mysqli_num_rows($result);
+    echo 'first lesson end';
 ?>
-<ul>
-    <?php
-        while ($iter = mysqli_fetch_assoc($result) ) {
-            echo '<li>' . $iter['title'] . '</li>';
-        }
-    ?>
-</ul>
-<?php mysqli_close($connection); ?>
 
 </body>
 </html>
