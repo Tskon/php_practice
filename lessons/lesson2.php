f5e2c4ff4727046b35c98460bb822b623cf3fbff
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-08-15 11:23:51 +0300

    lesson2 second task

--- lessons/lesson2.php
+++ lessons/lesson2.php
@@ -1,4 +1,10 @@
 <?php
+/*
+first task
+если $a и $b положительные, вывести их разность;
+если $а и $b отрицательные, вывести их произведение;
+если $а и $b разных знаков, вывести их сумму;
+*/
 
 $a = rand(-100, 100);
 $b = rand(-100, 100);
@@ -13,3 +19,30 @@ if ($a >= 0 && $b >= 0) {
     echo 'a + b: '.($a + $b);
 }
 
+/*
+second part
+Присвоить переменной $а значение в промежутке [0..15]. С помощью оператора switch организовать вывод чисел от $a до 15.
+*/
+echo '<br> second part<br>';
+
+$a = rand(0, 15);
+echo "a = $a<br>";
+
+switch ($a){
+    case 0: echo '0<br>';
+    case 1: echo '1<br>';
+    case 2: echo '2<br>';
+    case 3: echo '3<br>';
+    case 4: echo '4<br>';
+    case 5: echo '5<br>';
+    case 6: echo '6<br>';
+    case 7: echo '7<br>';
+    case 8: echo '8<br>';
+    case 9: echo '9<br>';
+    case 10: echo '10<br>';
+    case 11: echo '11<br>';
+    case 12: echo '12<br>';
+    case 13: echo '13<br>';
+    case 14: echo '14<br>';
+    case 15: echo '15';
+}
