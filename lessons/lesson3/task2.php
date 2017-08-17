5bfc931b444c15dbe5ca623f260cf4333149e8d7
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-08-17 23:27:22 +0300

    lesson3 task 2

--- lessons/lesson3/task2.php
+++ lessons/lesson3/task2.php
@@ -2,5 +2,8 @@
 
 $i = 0;
 do {
-    if($i == 0) echo "$i - это ноль";
-} while (i <= 10)
+    if($i == 0) echo "$i - это ноль<br>";
+    elseif($i % 2 == 0) echo "$i - это четное число<br>";
+    else echo "$i - это нечетное число<br>";
+    $i++;
+} while ($i <= 10);
