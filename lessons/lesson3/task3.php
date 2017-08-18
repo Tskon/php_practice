70e9ce65d65deeef663777db57a67929e090cf3d
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-08-18 22:13:15 +0300

    lesson3 task3

--- lessons/lesson3/task3.php
+++ lessons/lesson3/task3.php
@@ -1,1 +1,15 @@
 <?php
+
+$region = [
+    'Московская область' => ['Москва', 'Зеленоград', 'Клин'],
+    'Санкт-Петербург' => ['Всеволжск', 'Павловск', 'Кронштадт']
+];
+
+printArr($region);
+
+function printArr($arr){
+    foreach ($arr as $key => $val) {
+        echo "$key:<br>";
+        echo implode(', ', $val) . '<br>';
+    }
+}
