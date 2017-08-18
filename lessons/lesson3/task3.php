6ae449323beb352dfe6dc7bcc230bca7cf13f5eb
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-08-19 00:06:46 +0300

    lesson3 task5

--- lessons/lesson3/task3.php
+++ lessons/lesson3/task3.php
@@ -1,15 +1,15 @@
 <?php
 
 $region = [
-    'Московская область' => ['Москва', 'Зеленоград', 'Клин'],
-    'Санкт-Петербург' => ['Всеволжск', 'Павловск', 'Кронштадт']
+  'Московская область' => ['Москва', 'Зеленоград', 'Клин'],
+  'Санкт-Петербург'    => ['Всеволжск', 'Павловск', 'Кронштадт']
 ];
 
 printArr($region);
 
-function printArr($arr){
-    foreach ($arr as $key => $val) {
-        echo "$key:<br>";
-        echo implode(', ', $val) . '<br>';
-    }
+function printArr($arr) {
+  foreach ($arr as $key => $val) {
+    echo "$key:<br>";
+    echo implode(', ', $val) . '<br>';
+  }
 }
--- lessons/lesson3/task5.php
+++ lessons/lesson3/task5.php
@@ -1,1 +1,8 @@
 <?php
+
+echo changeSpaces('Нужно больше этих мягких французских булок', '_');
+
+function changeSpaces($str, $separator) {
+  $arr = explode(' ', $str);
+  return implode($separator, $arr);
+}
