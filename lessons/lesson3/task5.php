d90e006a3a2884f0f72e99690e60c3ee7d789c45
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-08-19 02:00:21 +0300

    lesson3 task9

--- lessons/lesson3/task5.php
+++ lessons/lesson3/task5.php
@@ -4,9 +4,9 @@
  Написать функцию, которая заменяет в строке пробелы на подчеркивания и возвращает видоизмененную строчку.
 */
 
-echo changeSpaces('Нужно больше этих мягких французских булок', '_');
+echo changeSpaces('Нужно больше этих мягких французских булок');
 
-function changeSpaces($str, $separator) {
+function changeSpaces($str, $separator = '_') {
   $arr = explode(' ', $str);
   return implode($separator, $arr);
 }
