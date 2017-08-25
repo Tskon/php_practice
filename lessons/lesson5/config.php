a476458b533f470d96c2ad3f25c1260a648f5858
Tskon <tsekhmeister.k@gmail.com>
2017-08-25 21:33:21 +0300

    lesson5. Add BD and print data on page

---
+++ lessons/lesson5/config.php
@@ -0,0 +1,8 @@
+<?php
+$dir = 'img/'; //path to main image directory
+$maxImgSize = 1000000; //bites
+$host = 'lesson5';
+$dbName = 'php_practice';
+$dbUser = 'root';
+$dbPass = '';
+
--- lessons/lesson5/index.php
+++ lessons/lesson5/index.php
@@ -1,9 +1,10 @@
 <?php
+include 'config.php';
 include 'preview-maker.php';
 if(isset($_FILES['picture'])) {
   if(preg_match('/[.](jpg)|(gif)|(png)$/', $_FILES['picture']['name'])) {
-    if($_FILES['picture']['size'] > 1000000){
-        echo 'Слишком большая картинка!';
+    if($_FILES['picture']['size'] > $maxImgSize){
+      echo 'Слишком большая картинка!';
     }else {
       $filename = $_FILES['picture']['name'];
       $source = $_FILES['picture']['tmp_name'];
@@ -16,34 +17,45 @@ if(isset($_FILES['picture'])) {
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>PHP lvl1. Lesson 5</title>
+  <meta charset="UTF-8">
+  <title>PHP lvl1. Lesson 5</title>
 </head>
 <body>
 
 <div class="slider">
-  <?php
 
-  $dir = 'img/';
+  <?php
+  $link = mysqli_connect($host, $dbUser, $dbPass, $dbName);
+  $sqlResult = mysqli_query($link, 'select * from `photo_gallery` order by `name` desc');
+
+  $imagesArr = array();
+  while($row = mysqli_fetch_assoc($sqlResult)){
+      $imagesArr[] = $row;
+  }
+  print_r($imagesArr);
+
+  mysqli_close($link);
+
   $files = scandir($dir);
   for ($i = 0; $i < count($files); $i++) {
     if ($files[$i] != '.' && $files[$i] != '..') {
       $path = $dir . $files[$i];
       $miniPath = $dir.'mini/'.$files[$i];
       if (!file_exists($miniPath)){
-        create_thumbnail($path, $miniPath, 100, 100);
+        @create_thumbnail($path, $miniPath, 100, 100);
       }
       echo "<a target='_blank' href='$path'><img src='$miniPath'></a>";
     }
   }
 
   ?>
+
 </div>
 <div class="uploader">
-    <form enctype="multipart/form-data" action="" method="post">
-        <input type="file" name="picture">
-        <input type="submit" value="Загрузить">
-    </form>
+  <form enctype="multipart/form-data" action="" method="post">
+    <input type="file" name="picture">
+    <input type="submit" value="Загрузить">
+  </form>
 </div>
 
 
