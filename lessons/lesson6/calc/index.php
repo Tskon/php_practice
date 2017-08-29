ce5e4d6cce7b055d429d92c2983627410dec61ef
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-08-29 18:59:06 +0300

    lesson6. Calc post + jquery(Ajax). add regexp

--- lessons/lesson6/calc/index.php
+++ lessons/lesson6/calc/index.php
@@ -24,45 +24,8 @@
 </div>
 
 <script src="../js/jquery-3.2.1.min.js"></script>
-<script>
-    'use strict';
-    var a = 0;
-    var b = 0;
-    var operation;
-    var inputNum = $('input[name = "input"]');
-    $('input[name = "plus"]').click(function () {operInput('plus')});
-    $('input[name = "minus"]').click(function () {operInput('minus')});
-    $('input[name = "mult"]').click(function () {operInput('mult')});
-    $('input[name = "div"]').click(function () {operInput('div')});
-    $('input[name = "result"]').click(result);
+<script src="calc.js">
 
-    function operInput(oper){
-        operation = oper;
-        a = parseInt(inputNum.val().replace(/\D+/g,""));
-        inputNum.val('');
-        $('.result_out').html(a);
-    }
-   
-    function result() {
-        b = parseInt($('input[name = "input"]').val().replace(/\D+/g,""));
-        inputNum.val(b);
-        if (operation === 'dev' && b === 0) {
-            $('.result_out').html('Нельзя делить на ноль');
-            return;
-        }
-        sendPost(operation, a, b);
-    }
-    function sendPost(operation, a, b) {
-        var str = 'operation=' + operation + '&num1=' + a + '&num2=' + b;
-        $.ajax({
-            type: 'post',
-            url: 'server.php',
-            data: str,
-            success: function (ansv) {
-                $('.result_out').html(ansv);
-            }
-        })
-    }
 </script>
 </body>
 </html>
