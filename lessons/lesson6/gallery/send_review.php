f6f70666a1f526189e7a2979ca6b0571d7f23b3e
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-08-29 21:45:46 +0300

    lesson6. gallery reviews. add secure.

--- lessons/lesson6/gallery/send_review.php
+++ lessons/lesson6/gallery/send_review.php
@@ -3,8 +3,8 @@ include "config.php";
 
 $link = mysqli_connect($host, $dbUser, $dbPass, $dbName) or die ('Не подключиться к базе данных');
 
-$name = $_POST['name'];
-$text = $_POST['text'];
+$name = mysqli_real_escape_string($link, (string)htmlspecialchars(strip_tags($_POST['name']))) ;
+$text = mysqli_real_escape_string($link, (string)htmlspecialchars(strip_tags($_POST['text'])));
 $sql = "INSERT INTO `reviews`(`name`, `text`) VALUES ('$name', '$text')";
 mysqli_query($link, $sql);
 $sql = "SELECT top 1 `date` FROM `reviews` ORDER BY id DESC";
