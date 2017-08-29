4faa305af24fe0e6f59f5b89dc8f1be56f3833d3
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-08-29 21:46:45 +0300

    lesson6. gallery reviews. add secure.

--- lessons/lesson6/gallery/send_review.php
+++ lessons/lesson6/gallery/send_review.php
@@ -10,6 +10,5 @@ mysqli_query($link, $sql);
 $sql = "SELECT top 1 `date` FROM `reviews` ORDER BY id DESC";
 $result = mysqli_fetch_assoc(mysqli_query($link, $sql));
 mysqli_close($link);
-
 //print_r($result);
 echo $result['date'];
