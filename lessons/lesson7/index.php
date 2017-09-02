3bace297fb0c5bd43c41e8df30bc71d2d6855f4b
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-09-03 00:47:32 +0300

    lesson8 e-shop. start page content

---
+++ lessons/lesson7/index.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/main.css">
+		<title>e-shop</title>
+</head>
+<body>
+<div class="header">
+    <div class="basket">
+        <div>Товаров в корзине: <?=$count ?></div>
+    </div>
+</div>
+
+<div class="content">
+    <div class="item">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis ex fugiat nostrum praesentium qui. Aspernatur autem beatae dolore et ipsam maiores minima nulla odit, pariatur perspiciatis praesentium quod, tempora temporibus.
+        <input type="button" value="Цена: ">
+    </div>
+</div>
+<div class="content">
+    <div class="item">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis ex fugiat nostrum praesentium qui. Aspernatur autem beatae dolore et ipsam maiores minima nulla odit, pariatur perspiciatis praesentium quod, tempora temporibus.
+        <input type="button" value="Цена: ">
+    </div>
+</div>
+
+<script src="js/main.js"></script>
+</body>
+</html>
