6f6db3deb7102cf37e7fec0881ab7d3fcf761468
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-09-09 00:10:33 +0300

    lesson 8: connect first model with controller

--- lessons/lesson8/index.php
+++ lessons/lesson8/index.php
@@ -5,6 +5,7 @@
 // 1 Общие настройки
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+$modelName = null;
 
 // 2 Подключение файлов системы
 require_once 'config/config.php';
@@ -12,24 +13,28 @@ require_once 'config/config.php';
 // 3 Установка соединения с БД
 $link = mysqli_connect($host, $dbUser, $dbPass, $dbName);
 
-// 4 Вызов нужной модели
+// 4 парсинг адресной строки
 $uri = trim($_SERVER['REQUEST_URI'], '/');
-$uriArr = explode('/',$uri);
+$uriArr = explode('/', $uri);
 
-if(count($uriArr) > 0){
+if (count($uriArr) > 0) {
 	array_shift($uriArr); // убираем index.php
 }
-if(count($uriArr) > 0){
+if (count($uriArr) > 0) {
 	$modelName = array_shift($uriArr);
 }
-if(count($uriArr) > 0){
+if (count($uriArr) > 0) {
 	$actionName = array_shift($uriArr);
 }
-if(count($uriArr) > 0){
+if (count($uriArr) > 0) {
 	$params = $uriArr;
 }
 
-// 4 Вызов нужной модули
+// 5 Вызов нужной модели
+if ($modelName != null) {
+	include_once 'models/' . $modelName . '.php';
+	getProductsList($link);
+}
 
 
 mysqli_close($link);
--- lessons/lesson8/models/products.php
+++ lessons/lesson8/models/products.php
@@ -1,6 +1,11 @@
 <?php
-function getProductsList(){
 
+function getProductsList($link){
+	$sql = 'SELECT * FROM `products`';
+	$result = mysqli_query($link, $sql);
+	while($row = mysqli_fetch_assoc($result)){
+		print_r($row);
+	}
 }
 
-mysqli_close($link);
+echo 'models/products.php';
