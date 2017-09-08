1543fc37b576a003594c4abe73c5d0d83ebc276d
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-09-08 03:31:11 +0300

    lesson 8: basic MVC

--- lessons/lesson8/index.php
+++ lessons/lesson8/index.php
@@ -1,53 +1,19 @@
 <?php
-include 'tools/config.php';
 
-$link = mysqli_connect($host, $dbUser, $dbPass, $dbName);
-$sql = "SELECT * FROM `products` ORDER BY `name`";
-$result = mysqli_query($link, $sql);
+// front controller
 
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/main.css">
-    <title>e-shop</title>
-</head>
-<body>
-<div class="header">
-    <form target="_blank" method="post" action="auth.php">
-        <input type="text" name="login" placeholder="login" required>
-        <input type="password" name="pass" placeholder="password" required>
-        <button>Войти</button>
-        <a href="registration.php" target="_blank">Зарегистрироваться</a>
-    </form>
-    <div class="basket">
-        <h3>Товары в корзине: </h3>
-        <ul></ul>
-        <div>На сумму <div id="totalCoast"></div> р.</div>
-    </div>
-</div>
+// 1 Общие настройки
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-<div class="content">
-	<?php
-	while ($row = mysqli_fetch_assoc($result)) {
-		$productId = $row['id'];
-		$productName = $row['name'];
-		$productDescription = $row['description'];
-		$productCoast = $row['coast'];
-		echo
-		"<div class='item'>
-            <h3>$productName</h3>
-            <p>$productDescription</p>
-            <button class='to_basket_button' id='button-".$productId."'>В корзину: $productCoast р.</button>
-        </div>";
-	}
-	
-	?>
-</div>
+// 2 Подключение файлов системы
+
+define('ROOT', dirname(__FILE__));
+require_once 'components/Router.php';
+
+// 3 Установка соединения с БД
 
 
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="js/main.js"></script>
-</body>
-</html>
+// 4 Вызов Router
+$router = new Router();
+$router -> run();
