53341d8e100f4e906da1b2970b76de99d8667a4e
Tskon <tsekhmeister.k@gmail.com>
2018-06-14 22:27:21 +0300

    add structure

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
 </head>
 <body>
 <?php
-//phpinfo();
+phpinfo();
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-echo $foo;
+
+
 ?>
 
 </body>
