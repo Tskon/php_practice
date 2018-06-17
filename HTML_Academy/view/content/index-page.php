df6acc0427b239997680eb53773ce1f4231d6da0
Tskon <tsekhmeister.k@gmail.com>
2018-06-17 18:15:20 +0300

    hw3 task1: fix vars in index

--- HTML_Academy/view/content/index-page.php
+++ HTML_Academy/view/content/index-page.php
@@ -28,14 +28,14 @@
     <h2>Открытые лоты</h2>
   </div>
   <ul class="lots__list">
-    <?php foreach ($products as $val) { ?>
+    <?php foreach ($data['products'] as $val) { ?>
       <li class="lots__item lot">
         <div class="lot__image">
           <img src="<?= $val['imgUrl'] ?>" width="350" height="260"
-               alt="<?= $categories[$val['categoryId']] ?>">
+               alt="<?= $data['categories'][$val['categoryId']] ?>">
         </div>
         <div class="lot__info">
-          <span class="lot__category"><?= $categories[$val['categoryId']] ?></span>
+          <span class="lot__category"><?= $data['categories'][$val['categoryId']] ?></span>
           <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $val['title'] ?></a>
           </h3>
           <div class="lot__state">
