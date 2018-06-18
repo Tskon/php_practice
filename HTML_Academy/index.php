4ec36d7ebe7cdeb67e30b021c389580175b008f7
Tskon <tsekhmeister.k@gmail.com>
2018-06-18 16:32:14 +0300

    hw3 task1: replace error settings

--- HTML_Academy/index.php
+++ HTML_Academy/index.php
@@ -1,7 +1,3 @@
 <?php
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
 require_once 'config.php';
 require_once $path.'controller/main.php';
