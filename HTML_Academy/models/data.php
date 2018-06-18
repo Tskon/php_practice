2a03388e288a59530568b14b97c02016ef88beb9
Tskon <tsekhmeister.k@gmail.com>
2018-06-18 16:32:45 +0300

    hw3 task1: add new data funcs

--- HTML_Academy/models/data.php
+++ HTML_Academy/models/data.php
@@ -1,4 +1,5 @@
 <?php
+
 function getCategories() {
   return $categories = ['Доски и лыжи', 'Крепления', 'Ботинки', ' Одежда', 'Инструменты', 'Разное'];
 }
@@ -23,28 +24,33 @@ function getUserData() {
 }
 
 // ставки пользователей, которыми надо заполнить таблицу
-$bets = [
-  ['name' => 'Иван', 'price' => 11500, 'ts' => strtotime('-' . rand(1, 50) .' minute')],
-  ['name' => 'Константин', 'price' => 11000, 'ts' => strtotime('-' . rand(1, 18) .' hour')],
-  ['name' => 'Евгений', 'price' => 10500, 'ts' => strtotime('-' . rand(25, 50) .' hour')],
-  ['name' => 'Семён', 'price' => 10000, 'ts' => strtotime('last week')]
-];
+function getBets(){
+  return [
+    ['name' => 'Иван', 'price' => 11500, 'ts' => strtotime('-' . rand(1, 50) .' minute')],
+    ['name' => 'Константин', 'price' => 11000, 'ts' => strtotime('-' . rand(1, 18) .' hour')],
+    ['name' => 'Евгений', 'price' => 10500, 'ts' => strtotime('-' . rand(25, 50) .' hour')],
+    ['name' => 'Семён', 'price' => 10000, 'ts' => strtotime('last week')]
+  ];
+}
+
 
 // пользователи для аутентификации
-$users = [
-  [
-    'email' => 'ignat.v@gmail.com',
-    'name' => 'Игнат',
-    'password' => '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka'
-  ],
-  [
-    'email' => 'kitty_93@li.ru',
-    'name' => 'Леночка',
-    'password' => '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa'
-  ],
-  [
-    'email' => 'warrior07@mail.ru',
-    'name' => 'Руслан',
-    'password' => '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW'
-  ]
-];
+function getUsers(){
+  return [
+    [
+      'email' => 'ignat.v@gmail.com',
+      'name' => 'Игнат',
+      'password' => '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka'
+    ],
+    [
+      'email' => 'kitty_93@li.ru',
+      'name' => 'Леночка',
+      'password' => '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa'
+    ],
+    [
+      'email' => 'warrior07@mail.ru',
+      'name' => 'Руслан',
+      'password' => '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW'
+    ]
+  ];
+}
