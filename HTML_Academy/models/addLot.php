bcfac7ed6e4dd6b75d8279de2c4252d06d1522b6
Tskon <tsekhmeister.k@gmail.com>
2018-06-29 19:48:38 +0300

    hw4 task2: add comments

--- HTML_Academy/models/addLot.php
+++ HTML_Academy/models/addLot.php
@@ -2,7 +2,7 @@
 $fields = ['category', 'lot-name', 'message', 'image', 'lot-rate', 'lot-step', 'lot-date'];
 
 foreach ($fields as $field) {
-  if(isset($_POST[$field])){
+  if (isset($_POST[$field])) {
     var_dump($_POST[$field]);
   } elseif (isset($_FILES[$field])) {
     $fileType = $_FILES[$field]['type'];
@@ -10,7 +10,9 @@ foreach ($fields as $field) {
     $fileSize = $_FILES[$field]['size'];
     $fileTmpName = $_FILES[$field]['tmp_name'];
     var_dump(strpos($fileType, "image") !== false);
-//    $path;
+//    $path; // todo добавить сохранение файла.
+  } else {
+    // todo тогда добавить этот файл в список ошибок.
   }
   print '<br/>';
 }
