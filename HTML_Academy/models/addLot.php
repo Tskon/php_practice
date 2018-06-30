46421e76f19abc03f23edee3691c9f773e8c8b27
Tskon <tsekhmeister.k@gmail.com>
2018-06-30 22:27:13 +0300

    hw4 task2: add uploading file

--- HTML_Academy/models/addLot.php
+++ HTML_Academy/models/addLot.php
@@ -3,14 +3,19 @@ $fields = ['category', 'lot-name', 'message', 'image', 'lot-rate', 'lot-step', '
 
 foreach ($fields as $field) {
   if (isset($_POST[$field])) {
-    var_dump($_POST[$field]);
   } elseif (isset($_FILES[$field])) {
     $fileType = $_FILES[$field]['type'];
     $fileName = $_FILES[$field]['name'];
     $fileSize = $_FILES[$field]['size'];
     $fileTmpName = $_FILES[$field]['tmp_name'];
-    var_dump(strpos($fileType, "image") !== false);
-//    $path; // todo добавить сохранение файла.
+    if(strpos($fileType, "image") !== false){
+      $uploadDir = $path.'img/upload/';
+      $uploadFile = $uploadDir . basename($fileName);
+      print $fileTmpName;
+      print '<br>';
+      print $uploadFile;
+      move_uploaded_file($fileTmpName, $uploadFile);
+    }
   } else {
     // todo тогда добавить этот файл в список ошибок.
   }
