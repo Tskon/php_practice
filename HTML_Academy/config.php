1aebdba23ec23abc74f36742cd22a16da6dce9b2
Tskon <tsekhmeister.k@gmail.com>
2018-07-01 18:18:04 +0300

    hw4 task2: add file size to config

--- HTML_Academy/config.php
+++ HTML_Academy/config.php
@@ -2,6 +2,7 @@
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+ini_set('upload_max_filesize', "10M") ;
 
 date_default_timezone_set('Europe/Moscow');
 
