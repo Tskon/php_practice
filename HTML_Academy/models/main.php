3cd2774bb6b788263f16b26fc6e52adbe1b5a6c4
Tskon <tsekhmeister.k@gmail.com>
2018-07-01 17:07:43 +0300

    hw4 task2: fix for upload

--- HTML_Academy/models/main.php
+++ HTML_Academy/models/main.php
@@ -8,19 +8,19 @@ function getFormatedCoast($amount) {
   return number_format(ceil($amount), 0, '.', ' ') . '<b class="rub">р</b>';
 }
 
-function requireToVar($file, $data = []){
+function requireToVar($file, $data = []) {
   ob_start();
   require($file);
   return ob_get_clean();
 }
 
-function timeToMidnight(){
+function timeToMidnight() {
   $now = new DateTime();
-  $toMidnight = strtotime('tomorrow') - $now -> getTimestamp();
-  return ($toMidnight/3600 % 24) . ':' . ($toMidnight/60 % 60);
+  $toMidnight = strtotime('tomorrow') - $now->getTimestamp();
+  return ($toMidnight / 3600 % 24) . ':' . ($toMidnight / 60 % 60);
 }
 
-function fileSave($fieldName, $options){
+function fileSave($fieldName, $options) {
   if (isset($_FILES[$fieldName])) {
     $errors = [];
 
@@ -28,19 +28,21 @@ function fileSave($fieldName, $options){
     $fileName = $_FILES[$fieldName]['name'];
     $fileSize = $_FILES[$fieldName]['size'];
     $fileTmpName = $_FILES[$fieldName]['tmp_name'];
-    print($fileSize . ' size '. $fileName);
 
     if (isset($options['type']) && strpos($fileType, $options['type']) === false) {
       $errors[] = 'Incorrect file type';
     }
-    if (isset($options['maxSize']) && $fileSize > ($options['maxSize'] * 1048576)){
-      $errors[] = 'The file exceeds '. $options['maxSize'].'mb';
+    if (isset($options['maxSize']) && $fileSize > ($options['maxSize'] * 1048576)) {
+      $errors[] = 'The file exceeds ' . $options['maxSize'] . 'mb';
     }
 
-    if(count($errors) === 0){
+    if (count($errors) === 0) {
       $uploadFile = $options['directory'] . basename($fileName);
       move_uploaded_file($fileTmpName, $uploadFile);
-      return ["type" => "success"];
+      return [
+        'type' => 'success',
+        'file' => $uploadFile
+      ];
     } else {
       return [
         "type" => "error",
