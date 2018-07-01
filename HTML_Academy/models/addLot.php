fac43018d68fd38f5f82fe2e2f3db676fbf6c8cb
Tskon <tsekhmeister.k@gmail.com>
2018-07-01 17:08:23 +0300

    hw4 task2: replace file upload logic to function

--- HTML_Academy/models/addLot.php
+++ HTML_Academy/models/addLot.php
@@ -3,21 +3,14 @@ $fields = ['category', 'lot-name', 'message', 'image', 'lot-rate', 'lot-step', '
 
 foreach ($fields as $field) {
   if (isset($_POST[$field])) {
-  } elseif (isset($_FILES[$field])) {
-    $fileType = $_FILES[$field]['type'];
-    $fileName = $_FILES[$field]['name'];
-    $fileSize = $_FILES[$field]['size'];
-    $fileTmpName = $_FILES[$field]['tmp_name'];
-    if(strpos($fileType, "image") !== false){
-      $uploadDir = $path.'img/upload/';
-      $uploadFile = $uploadDir . basename($fileName);
-      print $fileTmpName;
-      print '<br>';
-      print $uploadFile;
-      move_uploaded_file($fileTmpName, $uploadFile);
-    }
+
   } else {
-    // todo тогда добавить этот файл в список ошибок.
+    $result = fileSave($field, ['directory' => $path . 'img/upload/', 'type' => 'image', 'maxSize' => 3]);
+
+    if ($result['type'] === 'success') {
+      print '<img src="'. $uploadImgPath . basename($result['file']) . '"/>';
+    } else {
+      print '<br/>' . implode(', ', $result['errors']);
+    }
   }
-  print '<br/>';
 }
