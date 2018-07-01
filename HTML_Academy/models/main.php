a1217511c481a5865b29e738fb4eae5496823495
Tskon <tsekhmeister.k@gmail.com>
2018-07-01 16:19:19 +0300

    hw4 task2: add upload file function

--- HTML_Academy/models/main.php
+++ HTML_Academy/models/main.php
@@ -19,3 +19,33 @@ function timeToMidnight(){
   $toMidnight = strtotime('tomorrow') - $now -> getTimestamp();
   return ($toMidnight/3600 % 24) . ':' . ($toMidnight/60 % 60);
 }
+
+function fileSave($fieldName, $options){
+  if (isset($_FILES[$fieldName])) {
+    $errors = [];
+
+    $fileType = $_FILES[$fieldName]['type'];
+    $fileName = $_FILES[$fieldName]['name'];
+    $fileSize = $_FILES[$fieldName]['size'];
+    $fileTmpName = $_FILES[$fieldName]['tmp_name'];
+    print($fileSize . ' size '. $fileName);
+
+    if (isset($options['type']) && strpos($fileType, $options['type']) === false) {
+      $errors[] = 'Incorrect file type';
+    }
+    if (isset($options['maxSize']) && $fileSize > ($options['maxSize'] * 1048576)){
+      $errors[] = 'The file exceeds '. $options['maxSize'].'mb';
+    }
+
+    if(count($errors) === 0){
+      $uploadFile = $options['directory'] . basename($fileName);
+      move_uploaded_file($fileTmpName, $uploadFile);
+      return ["type" => "success"];
+    } else {
+      return [
+        "type" => "error",
+        "errors" => $errors
+      ];
+    }
+  }
+}
