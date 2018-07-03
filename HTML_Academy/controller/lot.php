a21933f8850d15a5c82c397800fea52c41e2a928
Tskon <tsekhmeister.k@gmail.com>
2018-07-03 19:54:41 +0300

    hw5 task1: add handler for create cookie

--- HTML_Academy/controller/lot.php
+++ HTML_Academy/controller/lot.php
@@ -1,15 +1,22 @@
 <?php
-if (isset($query['id'])){
+global $query;
+
+if (isset($query['id'])) {
   $products = getProducts();
-  if(isset($products[$query['id'] - 1])){
+  if (isset($products[$query['id'] - 1])) {
+    if(isset($_COOKIE['history'])){
+      $history = json_decode($_COOKIE['history'], true);
+    }
+    $history[($query['id'] - 1)] = true;
+    setcookie('history', json_encode($history), strtotime('+1 day'), '/');
     $lotContent = renderPage($path . 'view/content/lot.php', [
       "products" => getProducts(),
       'categories' => getCategories(),
-      "id" => $query['id']
+      "id" => $query['id'] - 1
     ]);
     $html = renderPage($path . 'view/mainTemplate.php', ["content" => $lotContent, 'categories' => getCategories()]);
     print($html);
-  } else{
+  } else {
     header('Location: /404');
     exit;
   }
