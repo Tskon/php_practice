9e22bd30fa1c470e5d1c51aea8d0df041e8fe2a2
Tskon <tsekhmeister.k@gmail.com>
2018-07-07 22:50:57 +0300

    hw5 task2: simple login validation complete

--- HTML_Academy/models/login.php
+++ HTML_Academy/models/login.php
@@ -1,2 +1,29 @@
 <?php
-echo 'Login validation must be here';
+
+require_once $path . 'models/data.php';
+
+$users = getUsers();
+
+$email = ($_POST['email']) ? killDangerString($_POST['email'], 20) : null;
+$password = ($_POST['password']) ? killDangerString($_POST['password'], 30) : null;
+
+if ($email !== null && $password !== null){
+  $currentUser = null;
+
+  foreach ($users as $user) {
+    if ($user['email'] === $email) {
+      $currentUser = $user;
+      break;
+    }
+  }
+
+  if ($currentUser !== null && password_verify($password, $currentUser['password'])){
+    print 'Correct!';
+  } else {
+    print 'Incorrect password';
+  }
+
+} else {
+  print 'Введите корректные данные';
+}
+
